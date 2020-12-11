4e632235009b4df035545256ea9da7f466a7f600
Jef <jackefransham@gmail.com>
2020-12-11 17:36:00 +0100

    Add more to animation builder

--- skia-safe/src/animation.rs
+++ skia-safe/src/animation.rs
@@ -4,9 +4,27 @@ use std::{
     path::Path,
 };
 
-use crate::{prelude::*, Canvas, RCHandle, Rect};
+use crate::{prelude::*, Canvas, FontMgr, RCHandle, Rect};
 use skia_bindings as sb;
 
+bitflags::bitflags! {
+    /// Flags that affect behavior for when a [Builder] loads an [Animation].
+    #[derive(Default)]
+    pub struct BuilderFlags: u32 {
+        /// Normally, any static images are resolved at load time. This defers loading of images to
+        /// when you call [Animation::seek].
+        const DEFER_IMAGE_LOADING = sb::skottie_Animation_Builder_Flags_kDeferImageLoading as _;
+        /// By default Skia will use native typefaces when possible, but supplying this flag will cause
+        /// Skia to use the fallback glyph paths by default.
+        const PREFER_EMBEDDED_FONTS = sb::skottie_Animation_Builder_Flags_kPreferEmbeddedFonts as _;
+    }
+}
+
+/// Loader for [Animation], which allows you to supply the types necessary to load fonts
+/// and external assets, as well as allowing access to more advanced settings and hooks
+/// for affecting loading.
+///
+/// For simple files you can simply use `Animation::open` or `Animation::from_data`.
 #[repr(transparent)]
 pub struct Builder(sb::skottie_Animation_Builder);
 
@@ -35,8 +53,59 @@ impl Drop for Builder {
     }
 }
 
-/// A [Lottie](https://lottiefiles.com/) animation.
-pub type Animation = RCHandle<sb::skottie_Animation>;
+impl Builder {
+    /// Initialize a new animation builder with default flags.
+    pub fn new() -> Self {
+        Self::new_with_flags(Default::default())
+    }
+
+    /// Initialize a new animation builder, setting loading flags (see [BuilderFlags]).
+    pub fn new_with_flags(flags: BuilderFlags) -> Self {
+        Self(unsafe { sb::skottie_Animation_Builder::new(flags.bits()) })
+    }
+
+    /// Set the font manager that will be used to load fonts for any text used in the animation.
+    pub fn with_font_manager(&mut self, font_manager: FontMgr) -> &mut Self {
+        unsafe {
+            self.setFontManager(font_manager.into());
+        }
+
+        self
+    }
+
+    /// Parse the supplied .lottie file data and return an animation. Returns [None] if the data is
+    /// somehow invalid.
+    ///
+    /// Since Lottie files may reference external data, this function will also return [None] if
+    /// the file requests an external resource. If you want to be able to load external files,
+    /// see [Builder].
+    pub fn from_data(&mut self, data: &[u8]) -> Option<Animation> {
+        Animation::from_ptr(unsafe { self.make1(data.as_ptr() as *const i8, data.len()) }.fPtr)
+    }
+
+    /// Opens the .lottie file at the given path (expressed as a C string).
+    ///
+    /// Since Lottie files may reference external data, this function will also return [None] if
+    /// the file requests an external resource. If you want to be able to load external files,
+    /// see [Builder].
+    pub fn open_cstr<P: AsRef<CStr>>(&mut self, path: P) -> Option<Animation> {
+        Animation::from_ptr(unsafe { self.makeFromFile(path.as_ref().as_ptr()) }.fPtr)
+    }
+
+    /// Opens the .lottie file at the given path. This function must allocate in order to create
+    /// a C string from the path, use `open_cstr` if you want to avoid this. Returns [None] if the
+    /// file cannot be found or is somehow invalid.
+    ///
+    /// Since Lottie files may reference external data, this function will also return [None] if
+    /// the file requests an external resource. If you want to be able to load external files,
+    /// see [Builder].
+    pub fn open<P: AsRef<Path>>(&mut self, path: P) -> Option<Animation> {
+        let path = CString::new(path.as_ref().to_string_lossy().into_owned().into_bytes())
+            .expect("CString::new failed: path contains null bytes");
+
+        self.open_cstr(&path)
+    }
+}
 
 bitflags::bitflags! {
     #[derive(Default)]
@@ -45,6 +114,10 @@ bitflags::bitflags! {
         const DISABLE_TOP_LEVEL_CLIPPING = sb::skottie_Animation_RenderFlag::kDisableTopLevelClipping as _;
     }
 }
+
+/// A [Lottie](https://lottiefiles.com/) animation. If you need more advanced loading (such as automatically
+/// loading external assets) see [Builder].
+pub type Animation = RCHandle<sb::skottie_Animation>;
 
 impl NativeRefCounted for sb::skottie_Animation {
     fn _ref(&self) {
@@ -61,39 +134,39 @@ impl NativeRefCounted for sb::skottie_Animation {
 }
 
 impl Animation {
-    /// Parse the supplied .lottie file data and return an animation. Returns `None` if the data is
+    /// Parse the supplied .lottie file data and return an animation. Returns [None] if the data is
     /// somehow invalid.
     ///
-    /// Since Lottie files may reference external data, this function will also return `None` if
+    /// Since Lottie files may reference external data, this function will also return [None] if
     /// the file requests an external resource. If you want to be able to load external files,
-    /// see `Builder`.
+    /// see [Builder].
     pub fn from_data(data: &[u8]) -> Option<Self> {
         Self::from_ptr(
             unsafe { sb::skottie_Animation::Make(data.as_ptr() as *const i8, data.len()) }.fPtr,
         )
     }
 
+    /// Opens the .lottie file at the given path (expressed as a C string).
+    ///
+    /// Since Lottie files may reference external data, this function will also return [None] if
+    /// the file requests an external resource. If you want to be able to load external files,
+    /// see [Builder].
+    pub fn open_cstr<P: AsRef<CStr>>(path: P) -> Option<Self> {
+        Self::from_ptr(unsafe { sb::skottie_Animation::MakeFromFile(path.as_ref().as_ptr()) }.fPtr)
+    }
+
     /// Opens the .lottie file at the given path. This function must allocate in order to create
-    /// a C string from the path, use `open_cstr` if you want to avoid this. Returns `None` if the
+    /// a C string from the path, use `open_cstr` if you want to avoid this. Returns [None] if the
     /// file cannot be found or is somehow invalid.
     ///
-    /// Since Lottie files may reference external data, this function will also return `None` if
+    /// Since Lottie files may reference external data, this function will also return [None] if
     /// the file requests an external resource. If you want to be able to load external files,
-    /// see `Builder`.
+    /// see [Builder].
     pub fn open<P: AsRef<Path>>(path: P) -> Option<Self> {
         let path = CString::new(path.as_ref().to_string_lossy().into_owned().into_bytes())
             .expect("CString::new failed: path contains null bytes");
 
         Self::open_cstr(&path)
-    }
-
-    /// Opens the .lottie file at the given path (expressed as a C string).
-    ///
-    /// Since Lottie files may reference external data, this function will also return `None` if
-    /// the file requests an external resource. If you want to be able to load external files,
-    /// see `Builder`.
-    pub fn open_cstr<P: AsRef<CStr>>(path: P) -> Option<Self> {
-        Self::from_ptr(unsafe { sb::skottie_Animation::MakeFromFile(path.as_ref().as_ptr()) }.fPtr)
     }
 
     /// Render this animation to a canvas, optionally specifying the location on the canvas that
@@ -114,7 +187,7 @@ impl Animation {
 
     /// Render this animation to a canvas, optionally specifying the location on the canvas that
     /// the animation should be rendered to, and supplying flags affecting how the animation is
-    /// rendered (see documentation for `RenderFlags`).
+    /// rendered (see documentation for [RenderFlags]).
     pub fn render_with_flags(
         &self,
         canvas: &mut Canvas,
